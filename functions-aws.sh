22c0b85c7f73afe5f1668f31e248c742d7a488a0
David Abarca <d.abarca@sdu.nl>
2024-01-05 19:20:49 +0100

    Save config

--- functions-aws.sh
+++ functions-aws.sh
@@ -1,42 +1,66 @@
 #!/bin/zsh
 
-aws() {
-  : '
-  # What
-  Spawn container with latest tag from AWS official AWS CLI image.
+if [[ $(hostname -s) == "da-macbook-pro-mc" ]]; then
+  aws() {
+    : '
+    # What
+    Spawn container with latest tag from AWS official AWS CLI image.
 
-  # Usage
-  aws --version
-  '
+    # Usage
+    aws --version
+    '
 
-  docker run \
-  --rm \
-  --volume "$(PWD)":/src/ \
-  --workdir /src/ \
-  --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
-  --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
-  --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
-  --interactive \
-  amazon/aws-cli:latest "$@"
-}
+    docker run \
+    --rm \
+    --interactive \
+    --tty \
+    --volume "$(PWD)":/src/ \
+    --workdir /src/ \
+    --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+    --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+    --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
+    amazon/aws-cli:latest "$@"
+  }
 
-aws-eks-config-gen() {
-  : '
-  # What
-  Spawn container with latest tag from AWS official AWS CLI image.
+  aws-exec() {
+    : '
+    # What
+    Spawn container from image to build and test.
 
-  # Usage
-  aws-eks-config-gen --name dev-demo --region eu-west-1
-  '
+    # Usage
+    ansible-exec
+    '
 
-  docker run \
-  --rm \
-  --volume "$(PWD)":/src/ \
-  --volume $HOME/.kube/config:/root/.kube/config \
-  --workdir /src/ \
-  --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
-  --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
-  --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
-  --interactive \
-  amazon/aws-cli:latest eks update-kubeconfig "$@"
-}
+    docker run \
+    --rm \
+    --interactive \
+    --tty \
+    --volume "$(PWD)":/src/ \
+    --workdir /src/ \
+    --env AWS_CONFIG_FILE="/root/.aws/config" \
+    --env AWS_SHARED_CREDENTIALS_FILE="/root/.aws/credentials" \
+    --entrypoint /bin/sh \
+    amazon/aws-cli:latest
+  }
+
+  aws-eks-config-gen() {
+    : '
+    # What
+    Spawn container with latest tag from AWS official AWS CLI image.
+
+    # Usage
+    aws-eks-config-gen --name dev-demo --region eu-west-1
+    '
+
+    docker run \
+    --rm \
+    --volume "$(PWD)":/src/ \
+    --volume $HOME/.kube/config:/root/.kube/config \
+    --workdir /src/ \
+    --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+    --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+    --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
+    --interactive \
+    amazon/aws-cli:latest eks update-kubeconfig "$@"
+  }
+fi
--- functions-terraform.sh
+++ functions-terraform.sh
@@ -1,67 +1,68 @@
 #!/bin/zsh
+if [[ $(hostname -s) == "da-macbook-pro-mc" ]]; then
+  terraform-exec() {
+    : '
+    # What
+    Spawn container from image with tag terragrunt-local, created from terragrunt-build.
 
-terraform-exec() {
-  : '
-  # What
-  Spawn container from image with tag terragrunt-local, created from terragrunt-build.
+    # Usage
+    terragrunt-exec
+    '
 
-  # Usage
-  terragrunt-exec
-  '
-
-  docker run \
-	--rm \
-	--interactive \
-	--tty \
-	--network host \
-	--volume "$(PWD)":/src/ \
-	--workdir /src/ \
-	--entrypoint /bin/sh \
-  --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
-  --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
-  --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
-	davaba/terraform:latest
-}
-
-terraform() {
-  : '
-  # What
-  Spawn container with specific TF version from Hashicorp official TF image.
-
-  # Usage
-  terraform --version
-  terraform plan
-  terraform apply
-  terraform destroy
-  '
-
-  docker run \
-  --rm \
-  --interactive \
-  --volume "$(PWD)":/src/ \
-  --workdir /src/ \
-  --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
-  --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
-  --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
-  davaba/terraform:latest terraform "$@"
-}
-
-terragrunt() {
-  : '
-  # What
-  Spawn container from image with tag terragrunt-local in order to run terragrunt with arguments.
-
-  # Usage
-  terragrunt --version
-  '
-
-  docker run \
+    docker run \
     --rm \
     --interactive \
-    --volume "$(PWD)"/:/src/ \
+    --tty \
+    --network host \
+    --volume "$(PWD)":/src/ \
+    --workdir /src/ \
+    --entrypoint /bin/sh \
+    --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+    --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+    --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
+    davaba/terraform:latest
+  }
+
+  terraform() {
+    : '
+    # What
+    Spawn container with specific TF version from Hashicorp official TF image.
+
+    # Usage
+    terraform --version
+    terraform plan
+    terraform apply
+    terraform destroy
+    '
+
+    docker run \
+    --rm \
+    --interactive \
+    --volume "$(PWD)":/src/ \
     --workdir /src/ \
     --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
     --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
     --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
-    davaba/terraform:latest terragrunt "$@"
-}
+    davaba/terraform:latest terraform "$@"
+  }
+
+  terragrunt() {
+    : '
+    # What
+    Spawn container from image with tag terragrunt-local in order to run terragrunt with arguments.
+
+    # Usage
+    terragrunt --version
+    '
+
+    docker run \
+      --rm \
+      --interactive \
+      --volume "$(PWD)"/:/src/ \
+      --workdir /src/ \
+      --env AWS_ACCESS_KEY_ID="${AWS_ACCESS_KEY_ID}" \
+      --env AWS_SECRET_ACCESS_KEY="${AWS_SECRET_ACCESS_KEY}" \
+      --env AWS_SESSION_TOKEN="${AWS_SESSION_TOKEN}" \
+      davaba/terraform:latest terragrunt "$@"
+  }
+fi
